f5d36dd0965fba98af1064d5671c615ec41474b9
JayeshGondaliya <jayeshgondaliya9929@gmail.com>
2024-08-09 18:21:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Textforms from './component/Textforms';
+import { useState } from 'react';
+import Alert from './component/Alert';
+import Contact from './component/Contact';
 
 function App() {
+  const [Mode, setMode] = useState("light")
+  let ModeClick = () => {
+    if (Mode == "light") {
+      setMode("dark")
+      document.body.style.backgroundColor = "black";
+      showAlerts("this is darkMode","danger")
+      document.title="Darkmode Enable"
+      setInterval(() => {
+      document.title="TextUtils App"
+
+      }, 2000);      
+    }
+    else {
+      setMode("light")
+      // document.body.style.backgroundColor = "white";
+      showAlerts("this is lightMode ","success")   
+         document.title="LightMode Enable"
+      setInterval(() => {
+      document.title="TextUtils App"
+
+      }, 2000);  
+    }
+  }
+
+  // alert
+  const [alert,setMyalerts]=useState(null);
+  let showAlerts=(msg,type)=>{
+    setMyalerts({
+      msg:msg,
+      type:type
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" Mode={Mode} ModeClick={ModeClick} />
+      <Alert alert={alert}/>
+      <Textforms />
+  
+    </>
   );
 }
 
